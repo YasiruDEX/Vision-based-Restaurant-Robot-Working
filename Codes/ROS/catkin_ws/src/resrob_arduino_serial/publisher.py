9d6288f3aa109e47d63ecb73140fa74de2272431
Herath HMSI <herathsandun21@gmail.com>
2024-03-30 13:09:57 +0000

    Create publisher.py
    Creating a node for publisher named 'messagepublisher' for ROS to communicate with Arduino through 'information' topic

---
+++ Codes/ROS/catkin_ws/src/resrob_arduino_serial/publisher.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import rospy
+#since we are sending int messages
+from std_msgs.msg import Int32
+
+nodeName='messagepublisher'
+
+#topic name
+topicName='information'
+
+rospy.init_node(nodeName, anonymous=True)
+
+publisher1=rospy.Publisher(topicName, Int32, queue_size=5)
+
+ratePublisher=rospy.Rate(1)
+intMessage=0
+
+
+while not rospy.is_shutdown():
+    rospy.loginfo(intMessage)
+    intMessage = int(input("Enter the command : "))
+    publisher1.publish(intMessage)
+    ratePublisher.sleep()
